9a3decacb40390a75410312a8ebbce6d098e839a
arun <arun1nly1@gmail.com>
2024-06-12 14:01:31 +0545

    delete + edit + get detail ofmovie

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import connectDB from "./db.connect.js";
 import Movie from "./movie.model.js";
+import mongoose from "mongoose";
 
 const app = express();
 
@@ -29,6 +30,98 @@ app.get("/movie/list", async (req, res) => {
 
   return res.status(200).send({ message: "success", movieList: movies });
 });
+
+// ?delete movie by id
+app.delete("/movie/delete/:id", async (req, res) => {
+  // extract id from req.params
+  const movieId = req.params.id;
+
+  // check for mongo id validity
+  const isValidMongoId = mongoose.isValidObjectId(movieId);
+
+  // if not valid mongo id, throw error
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // find movie using movie id
+  const movie = await Movie.findOne({ _id: movieId });
+
+  //  if not movie, throw error
+  if (!movie) {
+    return res.status(404).send({ message: "Movie does not exist." });
+  }
+
+  // delete movie
+  await Movie.deleteOne({ _id: movieId });
+
+  return res.status(200).send({ message: "Movie is deleted successfully." });
+});
+
+// ? get movie by id
+app.get("/movie/detail/:id", async (req, res) => {
+  // extract movie id from req.params
+  const movieId = req.params.id;
+
+  //  check for mongo id validity
+  const isValidMongoId = mongoose.isValidObjectId(movieId);
+
+  //  if not valid mongo id, throw error
+  if (!isValidMongoId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // find movie using movie id
+  const movie = await Movie.findOne({ _id: movieId });
+
+  //  if not movie, throw error
+  if (!movie) {
+    return res.status(404).send({ message: "Movie does not exist." });
+  }
+
+  // send movie as res
+  return res.status(200).send({ message: "success", movieDetail: movie });
+});
+
+// ? edit movie
+app.put("/movie/edit/:id", async (req, res) => {
+  // extract movie id from req.params
+  const movieId = req.params.id;
+
+  // check for mongo id validity
+  const isValidId = mongoose.isValidObjectId(movieId);
+
+  //  if not valid mongo id, throw error
+  if (!isValidId) {
+    return res.status(400).send({ message: "Invalid mongo id." });
+  }
+
+  // find movie by movie id
+  const movie = await Movie.findOne({ _id: movieId });
+
+  // if not movie, throw error
+  if (!movie) {
+    return res.status(404).send({ message: "Movie does not exist." });
+  }
+
+  // extract new values from req.body
+  const newValues = req.body;
+
+  // edit movie
+  await Movie.updateOne(
+    { _id: movieId },
+    {
+      $set: {
+        ...newValues,
+      },
+    }
+  );
+
+  // send res
+  return res.status(200).send({ message: "Movie is updated successfully." });
+});
+
+// app.get()
 // network port and server
 const PORT = 4000;
 
